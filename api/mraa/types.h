db40f71b46c9f9bcba20e2184f2a40a76b840125
zzl <zzl@radxa.com>
2020-08-25 09:22:36 +0800

    Add support for ROCK Pi S
    Signed-off-by: zzl <zzl@radxa.com>

--- api/mraa/types.h
+++ api/mraa/types.h
@@ -69,6 +69,7 @@ typedef enum {
     MRAA_UPXTREME = 24,             /**< The UPXTREME Board */
     MRAA_INTEL_ILK = 25,            /**< Intel Learning Kit */
     MRAA_SIEMENS_IOT2050 = 26,      /**< Siemens IOT2050 board */
+    MRAA_ROCKPIS = 27,              /**< Radxa ROCK PI S Board */
     // USB platform extenders start at 256
     MRAA_FTDI_FT4222 = 256,         /**< FTDI FT4222 USB to i2c bridge */
 
---
+++ include/arm/rockpis.h
@@ -0,0 +1,29 @@
+/*
+ * Author: zzl <zzl@radxa.com>
+ * Copyright (c) Radxa Limited.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mraa_internal.h"
+
+#define MRAA_ROCKPIS_GPIO_COUNT   17
+#define MRAA_ROCKPIS_I2C_COUNT    3
+#define MRAA_ROCKPIS_SPI_COUNT    1
+#define MRAA_ROCKPIS_UART_COUNT   3
+#define MRAA_ROCKPIS_PWM_COUNT    2
+#define MRAA_ROCKPIS_AIO_COUNT    1
+#define MRAA_ROCKPIS_PIN_COUNT    26
+
+mraa_board_t *
+        mraa_rockpis();
+
+#ifdef __cplusplus
+}
+#endif
